0c2d07c8617d0cc5f8c759701a15b51826d071c9
descartessainval <descartes.sainval@gmail.com>
2021-08-08 13:36:02 +0200

    add file triple.h

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "triple.h"
 
 int     main(int argc,char *argv[])
 {
---
+++ triple.h
@@ -0,0 +1,7 @@
+#ifndef TRIPLE_H_INCLUDED
+#define TRIPLE_H_INCLUDED
+
+int triple(int nombre);
+
+
+#endif
